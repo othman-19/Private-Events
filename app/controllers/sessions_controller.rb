63646c842fe1d337f1df71dec28afac601a3b104
othmane <othman-19@hotmail.fr>
2019-08-29 17:11:12 +0100

    fix linter errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,11 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:session][:name])
-    if user && user.name = (params[:session][:name])
+    if user && user.name == (params[:session][:name])
       session[:user_id] = user.id
       log_in user
       redirect_to user
     else
-      # Create an error message.
       render 'new'
     end
   end
