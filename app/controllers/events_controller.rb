3d6507da68fe10636d7237372a78d8809faca493
othmane <othman-19@hotmail.fr>
2019-09-10 18:23:51 +0100

    fix linter error

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -32,9 +32,6 @@ class EventsController < ApplicationController
   end
 
   def logged_in_user
-    unless logged_in?
-      flash[:danger] = 'Please log in.'
-      redirect_to login_path
-    end
+    redirect_to login_path unless logged_in?
   end
 end
